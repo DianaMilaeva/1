bc8c6077293258e3f24c5ead284fe57bad52bbfd
DianaMilaeva <dmilaeva64@gmail.com>
2026-04-07 05:38:05 +0300

    Add files via upload

---
+++ ConsoleApp6/FileName.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Drawing;
+using System.Security.Cryptography;
+
+abstract class Figure
+{
+    public abstract void Draw();
+    public abstract double GetArea();
+}
+
+class Circle : Figure
+{
+    private double radius;
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+    public override void Draw()
+    {
+        var text = """
+           *****
+         *       *
+        *         *
+         *       *
+           *****
+
+        """;
+        Console.WriteLine(text);
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle : Figure
+{
+    private double width;
+    private double height;
+    public Rectangle(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public override void Draw()
+    {
+        var text = """
+        ****
+        *  *
+        ****
+
+        """;
+        Console.WriteLine(text);
+    }
+
+    public override double GetArea()
+    {
+        return width * height;
+    }
+}
+
+class Square : Figure
+{
+    private double side;
+    public Square(double side)
+    {
+        this.side = side;
+    }
+    public override void Draw()
+    {
+        var text = """
+        ████████████
+        ██        ██
+        ██        ██
+        ██        ██
+        ████████████
+
+        """;
+        Console.WriteLine(text);
+    }
+    public override double GetArea()
+    {
+        return side * side;
+    }
+}
+
+class FigureManager
+{
+    public Figure CreateFigure(string type, params double[] parameters)
+    {
+        if (type == "circle")
+        {
+            return new Circle(parameters[0]);
+        } else if (type == "rectangle")
+        {
+            return new Rectangle(parameters[0], parameters[1]);
+        } else if (type == "square")
+        {
+            return new Square(parameters[0]);
+        }
+        else
+        {
+            throw new ArgumentException("Неизвестный тип фигуры");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FigureManager manager = new FigureManager();
+
+        Figure circle = manager.CreateFigure("circle", 3.0);
+        circle.Draw();
+        Console.WriteLine($"Площадь: {circle.GetArea():F2}");
+
+        Figure rect = manager.CreateFigure("rectangle", 4.0, 2.5);
+        rect.Draw();
+        Console.WriteLine($"Площадь: {rect.GetArea():F2}");
+
+        Figure square = manager.CreateFigure("square", 6.0);
+        square.Draw();
+        Console.WriteLine($"Площадь: {square.GetArea():F2}");
+    }
+}
+
+/*
+Класс Figure абстрактный родительский класс с чистыми виртуальными функциями (без реализации), задающий общий функционал для геометрических фигур.
+Его наследники круг, квадрат и прямоугольник реализуют конкретные правила: например, вычисляют площадь и рисуют фигуру.
+При вызове метода Draw через тип  Figure программа автоматически использует реализацию из соответствующего класса наследника
+*/
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,70 @@
+﻿/*using System;
+using System.Security.Cryptography;
+
+// абстрактный класс нельзя инстанциировать. Является основой для других классов
+abstract class User // var user = new User(); так делать нельзя будет ошибка
+{
+    public Guid id { get; } = Guid.NewGuid(); // глобальный идентификатор объекта
+    public string Name { get; set; }
+    public string Email { get; set; }
+    protected string PasswordHash { get; private set; }
+    public abstract void PerformAction(string src);
+
+    protected User (string Name, string Email, string Password)
+    {
+        Console.WriteLine($"Пользователь {Name} | {GetType().Name} вошёл в систему");
+        this.Name = Name;
+        this.Email = Email;
+        this.PasswordHash = HashPassword(Password);
+    }
+
+    public bool Login(string Password)
+    {
+        var inputHash = HashPassword(Password);
+        if (inputHash == PasswordHash) return true;
+        return false;
+    }
+    private string HashPassword(string password)
+    {
+        using (SHA256 sha =  SHA256.Create())
+        {
+            byte[] PasswordBytes = System.Text.Encoding.UTF8.GetBytes(password); // превратить строку пароля в байты
+            byte[] HashBytes = sha.ComputeHash(PasswordBytes); // передать байты в хеш-функцию, получить байты хеша
+            return Convert.ToBase64String(HashBytes); // конвертировать хеш в строковое представление
+        }
+    }
+}
+class AdminUser(string Name, String Email, string Password) : User(Name, Email, Password)
+{
+    public override void PerformAction(string src)
+    {
+        Console.WriteLine($"Пользователь {Name} с правами {GetType().Name} удалил ресурс {src}");
+    }
+}
+
+class CustomerUser(string Name, String Email, string Password) : User(Name, Email, Password)
+{
+    public override void PerformAction(string src)
+    {
+        Console.WriteLine($"Пользователь {Name} с правами {GetType().Name} прочитал ресурс {src}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        User[] users =
+        {
+            new AdminUser("Admin", "asrffh@pf.org", "adminpass"),
+            new CustomerUser("User1", "dfrgf", "user1pass"),
+            new CustomerUser("User2", "dfrgghjhjf", "user2pass"),
+            new CustomerUser("User3", "djiookhhf", "user3pass")
+        };
+        foreach (var user in users)
+        {
+            if (user.Login("adminpass")) user.PerformAction("пост 1");
+            if (user.Login("user3pass")) user.PerformAction("пост 3");
+        }
+    }
+}*/
