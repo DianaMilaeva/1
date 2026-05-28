88ccada56020166544997bd0439c1aaa4ccdbfe2
DianaMilaeva <dmilaeva64@gmail.com>
2026-05-28 13:07:03 +0300

    Add files via upload

---
+++ ConsoleApp35/21.cs
@@ -0,0 +1,33 @@
+﻿class Person
+{
+    private int _age;
+    public int Age
+    {
+        get { return _age; }
+        set
+        {
+            if (value < 0) Console.WriteLine("Ошибка: Возраст не может быть отрицательным");
+            else if (value > 120) Console.WriteLine("Ошибка: Возраст не может превышать 120 лет");
+            else _age = value;
+        }
+    }
+    public Person(int age)
+    {
+        Age = age; 
+    }
+    public void ShowInfo()
+    {
+        Console.WriteLine($"{Age} лет");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person1 = new Person(25);
+        person1.ShowInfo(); 
+        Person person2 = new Person(-5);  
+        person2.ShowInfo();
+    }
+}
