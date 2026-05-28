f25fa7bbd21d69033be1ca8e4e7590482287d90f
DianaMilaeva <dmilaeva64@gmail.com>
2026-05-28 13:09:08 +0300

    Add files via upload

---
+++ ConsoleApp35/22.cs
@@ -0,0 +1,35 @@
+﻿interface IPlayer
+{
+    void Play();
+    void Pause();
+    void Stop();
+}
+
+class MusicPlayer : IPlayer
+{
+    public void Play()
+    {
+        Console.WriteLine("Воспроизведение");
+    }
+
+    public void Pause()
+    {
+        Console.WriteLine("Пауза");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Остановка");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MusicPlayer mp = new MusicPlayer();
+        mp.Play();
+        mp.Pause();
+        mp.Stop();
+    }
+}
