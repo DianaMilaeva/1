ea78d5ad16005c15c8e6181c5f416dd230ee1540
DianaMilaeva <dmilaeva64@gmail.com>
2026-05-28 12:45:38 +0300

    Add files via upload

---
+++ ConsoleApp35/25.cs
@@ -0,0 +1,37 @@
+﻿abstract class Animal
+{
+    public int Age { get; set; }
+    public string Name { get; set; }
+    public abstract void MakeSound();
+}
+interface IFlyable
+{
+    void Fly();
+}
+class Lion : Animal
+{
+    public override void MakeSound() => Console.WriteLine("ррррр");
+}
+class Parrot : Animal, IFlyable
+{
+    public override void MakeSound() => Console.WriteLine("crack-crack");
+    public void Fly() => Console.WriteLine("Fly");
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Animal> animals = new List<Animal>();
+        animals.Add(new Parrot());
+        animals.Add(new Lion());
+        foreach (var animal in animals)
+        {
+            animal.MakeSound();
+            if (animal is IFlyable)
+            {
+                var animal_flyable = (IFlyable) animal;
+                animal_flyable.Fly();
+            }
+        }
+    }
+}
