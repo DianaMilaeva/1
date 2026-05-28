1c3ba703887ba52ac026acb6dbfe762fa3908f58
DianaMilaeva <dmilaeva64@gmail.com>
2026-05-28 13:12:40 +0300

    Add files via upload

---
+++ ConsoleApp35/23.cs
@@ -0,0 +1,37 @@
+﻿class Shape { }
+class Circle : Shape
+{
+    public void DrawCircle()
+    {
+        Console.WriteLine("круг");
+    }
+}
+class Square : Shape
+{
+    public void DrawSquare()
+    {
+        Console.WriteLine("квадрат");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Shape[] shapes = { new Circle(), new Square() };
+
+        foreach (Shape s in shapes)
+        {
+            if (s is Circle)
+            {
+                Circle c = (Circle)s;
+                c.DrawCircle();
+            }
+            else if (s is Square)
+            {
+                Square sq = (Square)s;
+                sq.DrawSquare();
+            }
+        }
+    }
+}
