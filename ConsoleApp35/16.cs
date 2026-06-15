3a291d0fa16f565d83edf513a06cffe7dac95d53
DianaMilaeva <dmilaeva64@gmail.com>
2026-06-15 20:37:36 +0300

    Update 16.cs

--- ConsoleApp35/16.cs
+++ ConsoleApp35/16.cs
@@ -1,32 +1,32 @@
 struct Coordinate
 {
-    public int X {  get; set; }
-    public int Y { get; set; }
+    public int X { get; set; }
+    public int Y {  get; set; }
     public Coordinate(int x, int y)
     {
-        X = x;
-        Y = y;
-    }
-    public Coordinate coordinate(int dx, int dy)
-    {
-        return new Coordinate(X + dx, Y + dy);
+        X = x; 
+        Y = y; 
     }
 }
 class Player
 {
-    public Coordinate position {  get; set; }
+    public Coordinate position;
+    public Player(int x, int y)
+    {
+        position = new Coordinate(x, y); 
+    }
     public void Move(int dx, int dy)
     {
-        position = new Coordinate(position.X + dx, position.Y + dy);
+        position.X += dx;
+        position.Y += dy;
     }
 }
-class Program 
+class Program
 {
     static void Main(string[] args)
     {
-        Player player = new Player();
-        player.position = new Coordinate(0, 0);
-        player.Move(6, 3);
+        Player player = new Player(0, 0);
+        player.Move(10, 1);
         Console.WriteLine($"X: {player.position.X}, Y: {player.position.Y}");
     }
 }
